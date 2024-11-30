fce3568e1dc95bc16dc9a1c88a44b50a7e6b49cb
Soumyo Ghosh <soumyo.ghosh24102003@gmail.com>
2024-11-30 19:59:34 +0530

    Add files via upload

--- webapp.py
+++ webapp.py
@@ -12,5 +12,6 @@ def query():
     return jsonify(results.to_dict())
 
 # Only needed for local testing; not used on Vercel
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
